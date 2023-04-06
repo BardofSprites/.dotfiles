095b6b23083e7dfb0a827ed92271526505f048f0
Bardman <bard@pop-os.localdomain>
2023-04-06 11:20:40 -0400

    Initial Commit

---
+++ doom/packages.el
@@ -0,0 +1,16 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+(package! copilot
+  :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+
+(package! gruber-darker-theme)
+
+(package! org-present)
+
+(package! org-roam)
+
+(package! org-roam-ui)
+
+(package! devdocs)
+
+(package! tao-theme)
